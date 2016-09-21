80f11e16c8f3b3e02e6a6ebf8c22d88e57403c54
Max Maischein <corion@corion.net>
2016-09-21 22:12:27 +0200

    Move MIME::Detect::Type into its own file

--- lib/MIME/Detect.pm
+++ lib/MIME/Detect.pm
@@ -5,6 +5,7 @@ use feature 'signatures';
 no warnings 'experimental::signatures';
 use Carp qw(croak);
 use XML::LibXML;
+use MIME::Detect::Type;
 
 use vars '$VERSION';
 $VERSION = '0.02';
@@ -220,7 +221,8 @@ sub parse_rule( $self, $rule ) {
     };
 
 Returns the list of MIME types according to their likelyhood.
-The first type is the most likely.
+The first type is the most likely. The returned objects
+are of type L<MIME::Detect::Type>.
 
 =cut
 
@@ -259,7 +261,7 @@ sub mime_types( $self, $file ) {
     print $type->mime_type, "\n"
         if $type;
 
-Returns the most likely type of a file. Returns C<undef>
+Returns the most likely type of a file as L<MIME::Detect::Type>. Returns C<undef>
 if no file type can be determined.
 
 =cut
@@ -337,261 +339,6 @@ sub request($self,$offset,$length) {
     } else {
         return ''
     };
-}
-
-1;
-
-package MIME::Detect::Type;
-use strict;
-use Moo;
-use if $] < 5.022, 'Filter::signatures';
-use feature 'signatures';
-no warnings 'experimental::signatures';
-
-=head1 NAME
-
-MIME::Detect::Type - the type of a file
-
-=head1 SYNOPSIS
-
-    my $type = $mime->mime_type('/usr/bin/perl');
-    print $type->mime_type;
-    print $type->comment;
-
-=head1 METHODS
-
-=cut
-
-=head2 C<< $type->aliases >>
-
-Reference to the aliases of this type
-
-=cut
-
-has 'aliases' => (
-    is => 'ro',
-    default => sub {[]},
-);
-
-=head2 C<< $type->comment >>
-
-Array reference of the type description in various languages
-(currently unused)
-
-=cut
-
-has 'comment' => (
-    is => 'ro',
-);
-
-=head2 C<< $type->mime_type >>
-
-    print "Content-Type: " . $type->mime_type . "\r\n";
-
-String of the content type
-
-=cut
-
-has 'mime_type' => (
-    is => 'ro',
-);
-
-=head2 C<< $type->globs >>
-
-    print $_ for @{ $type->globs };
-
-Arrayref of the wildcard globs of this type
-
-=cut
-
-has 'globs' => (
-    is => 'ro',
-    default => sub {[]},
-);
-
-sub _get_extension( $e=undef ) {
-    if( defined $e ) { $e =~ s!^\*\.!! };
-    $e
-}
-
-=head2 C<< $type->extension >>
-
-    print $type->extension; # pl
-
-Returns the default extension for this mime type, without a separating
-dot or the glob.
-
-=cut
-
-sub extension($self) { 
-    _get_extension( $self->globs->[0] );
-}
-
-=head2 C<< $type->valid_extension( $fn ) >>
-
-    print "$fn has the wrong extension"
-        unless $type->valid_extension( $fn );
-
-Returns whether C<$fn> matches one of the extensions
-as specified in C<globs>. If there is a match, the extension is returned.
-
-=cut
-
-sub valid_extension( $self, $fn ) {
-    _get_extension((grep {
-        my $g = $_;
-        $g =~ s![.]!\\.!g;
-        $g =~ s!\*!.*!g;
-        $fn =~ /$g$/;
-    } @{ $self->globs })[0])
-}
-
-=head2 C<< $type->priority >>
-
-    print $type->priority;
-
-Priority of this type. Types with higher priority
-get tried first when trying to recognize a file type.
-
-The default priority is 50.
-
-=cut
-
-has 'priority' => (
-    is => 'ro',
-    default => 50,
-);
-
-has 'rules' => (
-    is => 'ro',
-    default => sub { [] },
-);
-
-=head2 C<< $type->superclass >>
-
-    my $sc = $type->superclass;
-    print $sc->mime_type;
-
-The notional superclass of this file type. Note that superclasses
-don't necessarily match the same magic numbers.
-
-=cut
-
-has 'superclass' => (
-    is => 'rw',
-    default => undef,
-);
-
-sub BUILD($self, $args) {
-    # Preparse the rules here:
-    for my $rule (@{ $args->{rules} }) {
-        my $value = $rule->{value};
-
-        # This should go into the part reading the XML, not into the part
-        # evaluating the rules
-        if( ref $rule eq 'HASH' and $rule->{type} eq 'string' ) {
-            my %replace = (
-                'n' => "\n",
-                'r' => "\r",
-                't' => "\t",
-                "\\" => "\\",
-            );
-            $value =~ s{\\([nrt\\]|([0-7][0-7][0-7])|x([0-9a-fA-F][0-9a-fA-F]))}
-                       { $replace{$1} ? $replace{$1} 
-                       : $2 ? chr(oct($2))
-                       : $3 ? chr(hex($3))
-                       : $1
-                       }xge;
-
-        } elsif( ref $rule eq 'HASH' and $rule->{type} eq 'little32' ) {
-            $value = pack 'V', hex($rule->{value});
-
-        } elsif( ref $rule eq 'HASH' and $rule->{type} eq 'little16' ) {
-            $value = pack 'v', hex($rule->{value});
-
-        } elsif( ref $rule eq 'HASH' and $rule->{type} eq 'big32' ) {
-            $value = pack 'N', hex($rule->{value});
-
-        } elsif( ref $rule eq 'HASH' and $rule->{type} eq 'big16' ) {
-            $value = pack 'n', hex($rule->{value});
-
-        } elsif( ref $rule eq 'HASH' and $rule->{type} eq 'host16' ) {
-            $value = pack 'S', hex($rule->{value});
-
-        } elsif( ref $rule eq 'HASH' and $rule->{type} eq 'host32' ) {
-            $value = pack 'L', hex($rule->{value});
-
-        } elsif( ref $rule eq 'HASH' and $rule->{type} eq 'byte' ) {
-            $value = pack 'c', hex($rule->{value});
-
-        } else {
-            die "Unknown rule type '$rule->{type}'";
-        };
-
-        $rule->{type} = 'string';
-        $rule->{value} = $value;
-    }
-}
-
-sub compile($self,$fragment) {
-    die "No direct-to-Perl compilation implemented yet.";
-}
-
-=head2 C<< $type->matches $buffer >>
-
-    my $buf = "PK\003\004"; # first four bytes of file
-    if( $type->matches( $buf ) {
-        print "Looks like a " . $type->mime_type . " file";
-    };
-
-=cut
-
-sub matches($self, $buffer, $rules = $self->rules) {
-    my @rules = @$rules;
-
-    # Superclasses are for information only
-    #if( $self->superclass and $self->superclass->mime_type !~ m!^text/!) {
-    #    return if ! $self->superclass->matches($buffer);
-    #};
-
-    if( !ref $buffer) {
-        # Upgrade to an in-memory filehandle
-        my $_buffer = $buffer;
-        open my $fh, '<', \$_buffer
-            or die "Couldn't open in-memory handle!";
-        binmode $fh;
-        $buffer = MIME::Detect::Buffer->new(fh => $fh);
-    };
-
-    # Hardcoded rule for plain text detection...
-    if( $self->mime_type eq 'text/plain') {
-        my $buf = $buffer->request(0,256);
-        return $buf !~ /[\x00-\x08\x0b\x0c\x0e-\x1f]/;
-    };
-
-    my $matches;
-    for my $rule (@rules) {
-
-        my $value = $rule->{value};
-
-        my $buf = $buffer->request($rule->{offset}, length $value);
-        #use Data::Dumper;
-        #$Data::Dumper::Useqq = 1;
-        no warnings ('uninitialized', 'substr');
-        if( $rule->{offset} =~ m!^(\d+):(\d+)$! ) {
-            #warn sprintf "%s: index match %d:%d for %s", $self->mime_type, $1,$2, Dumper $value;
-            #warn Dumper substr( $buf, 0, ($2-$1)+length($value));
-            $matches = $matches || 1+index( substr( $buf, 0, ($2-$1)+length($value)), $value );
-        } else {
-            #warn sprintf "%s: substring match %d for %s", $self->mime_type, $rule->{offset}, Dumper $value;
-            #warn Dumper substr( $buf, $rule->{offset}, length($value));
-            $matches = $matches || substr( $buf, 0, length($value)) eq $value;
-        };
-        $matches = $matches && $self->matches( $buffer, $rule->{and} ) if $rule->{and};
-
-        last if $matches;
-    };
-    !!$matches
 }
 
 1;
